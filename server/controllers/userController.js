ceea77399ad9f1495cfce52cd4daa9df5cc7466d
Preethika <preethikasdev@gmail.com>
2025-05-23 21:25:24 +0530

    user signup and login done

---
+++ server/controllers/userController.js
@@ -0,0 +1,89 @@
+const User = require('../models/userModel')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+// signup
+
+const signUp = async (req, res) => {
+    try {
+        const { name, email, password, reTypePassword, bio } = req.body
+
+        if (!name) {
+            return res.status(400).json({ message: 'Please provide the username' })
+        }
+        if (!password) {
+            return res.status(400).json({ message: 'Please provide the password' })
+        }
+        if (!email) {
+            return res.status(400).json({ message: 'Please provide an email' })
+        }
+
+        if (password !== reTypePassword) {
+            return res.status(400).json({ message: 'Passwords do not match' });
+        }
+
+        const existingUser = await User.findOne({ email })
+        if (existingUser) {
+            return res.status(400).json({ message: "Existing User..Kindly Login" })
+        }
+
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const newUser = new User({
+            name, email, password: hashedPassword, bio
+        })
+
+        await newUser.save()
+        // alert("")
+        res.status(201).json({ message: 'Signup Sucessfull, Redirecting to login page' })
+
+
+    } catch (error) {
+        console.log('Login error:', error);
+        res.status(500).json({ message: 'Server Error' })
+    }
+}
+
+//login
+
+const login = async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        if (!password) {
+            return res.status(400).json({ message: 'Please provide the password' })
+        }
+        if (!email) {
+            return res.status(400).json({ message: 'Please provide an email' })
+        }
+
+        const existingUser = await User.findOne({ email })
+        if (!existingUser) {
+            return res.status(404).json({ message: 'User does not exist. Kindly Signup first' })
+        }
+
+        const passwordMatched = await bcrypt.compare(password, existingUser.password)
+        if (!passwordMatched) {
+            return res.status(401).json({ message: 'Invalid password' })
+        }
+
+        const token = jwt.sign({ userid: existingUser.id, email: existingUser.email }, process.env.JWT_SECRET, { expiresIn: '1d' })
+
+
+        res.status(200).json({
+            user: { userid: existingUser.id, name: existingUser.name, email: existingUser.email },
+            token,
+            message: `Login Sucess, Welcome to ATMOS- ${existingUser.name}`
+        })
+    } catch (error) {
+        res.status(500).json({ message: 'Server error.PLease try again' })
+
+
+    }
+
+
+
+}
+
+module.exports = { signUp, login }
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,23 +1,24 @@
 const express=require('express')
 const UserRouter=express.Router()
 const userController=require('../controllers/userController')
+const userAuth=require('../middlewares/userAuth')
 
 
 UserRouter.post('/signup',userController.signUp)
 UserRouter.post('/login',userController.login)
-UserRouter.get('/users/:id',userController.getUser)
-UserRouter.delete('/users/:id',userController.deleteAccount)
+UserRouter.get('/users/:id',userAuth,userController.getUser)
+UserRouter.delete('/users/:id',userAuth,userController.deleteAccount)
 
-UserRouter.post('/user/:id/favorites/:eventId',userController.setFavorite)
+UserRouter.post('/user/:id/favorites/:eventId',userAuth,userController.setFavorite)
 
-UserRouter.post('/user/:id/events/:eventId/comments',userController.postComment)
-UserRouter.get('/user/:id/comments',userController.previousComments)
-UserRouter.patch('/user/:id/events/:eventId/comments/:commentId',userController.editComment)
-UserRouter.delete('/user/:id/events/:eventId/comments/:commentId',userController.deleteComment)
+UserRouter.post('/user/:id/events/:eventId/comments',userAuth,userController.postComment)
+UserRouter.get('/user/:id/comments',userAuth,userController.previousComments)
+UserRouter.patch('/user/:id/events/:eventId/comments/:commentId',userAuth,userController.editComment)
+UserRouter.delete('/user/:id/events/:eventId/comments/:commentId',userAuth,userController.deleteComment)
 
-UserRouter.get('/user/:id/event/:eventId/notifications',userController.getNotification)
-UserRouter.put('/user/:id/event/:eventId/notifications/:notificationId',userController.markAsRead)
+UserRouter.get('/user/:id/event/:eventId/notifications',userAuth,userController.getNotification)
+UserRouter.put('/user/:id/event/:eventId/notifications/:notificationId',userAuth,userController.markAsRead)
 
-UserRouter.get('/events/status',userController.pendingApproval)
+UserRouter.get('/events/status',userAuth,userController.pendingApproval)
 
 module.exports=UserRouter
